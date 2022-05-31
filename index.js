e88b719d6a66c63099a91af5b404a89f7ad913d6
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-31 13:08:26 +0300

    chore: clean up

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const visit = require("unist-util-visit");
 const is = require("unist-util-is");
 
