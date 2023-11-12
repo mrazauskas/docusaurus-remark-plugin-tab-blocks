407bced824d1af1b8c6268e91939bad6180005bc
Tom Mrazauskas <tom@mrazauskas.de>
2023-11-12 20:23:55 +0200

    feat: add type definitions files

---
+++ typetests/index.tst.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "tstyche";
+import tabBlocks, { type Options } from "docusaurus-remark-plugin-tab-blocks";
+
+const options: Options = {};
+
+test("tabBlocks()", () => {
+  expect(tabBlocks()).type.toBeVoid();
+  expect(tabBlocks(options)).type.toBeVoid();
+});
+
+test("Options", () => {
+  expect(options).type.toBeAssignable({ groupId: "code-examples" });
+  expect(options).type.toBeAssignable({
+    labels: [
+      ["js", "JavaScript"] as [string, string],
+      ["ts", "TypeScript"] as [string, string],
+    ],
+  });
+  expect(options).type.toBeAssignable({ sync: true });
+});
